fab92e9b8149934e4c75ad2aca4ee753af9148da
Nikolai R Kristiansen <nikolaik@gmail.com>
2015-04-14 20:38:22 +0200

    Page from live

---
+++ app/webroot/files/en-by.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href='//fonts.googleapis.com/css?family=Arvo:700' rel='stylesheet' type='text/css' />
+<meta charset="utf-8" />
+<title>Én by. Ett studentersamfund.</title>
+<style type="text/css">
+body {
+        background:black;
+        color:#ff9e29;
+        margin:0;
+        padding:0;
+}
+#cto {
+        position:absolute;
+        top:50%;
+        margin-top:-3em;
+        font-family:Arvo,Arial,sans-serif;
+        line-height:1.4em;
+        font-size:50px;
+        text-align:center;
+        width:100%;
+        padding:0;
+}
+</style>
+</head>
+<body>
+
+<div id="cto">
+        <p>Én by</p>
+        <p>Ett studentersamfund</p>
+</div>
+
+</body>
+</html>
